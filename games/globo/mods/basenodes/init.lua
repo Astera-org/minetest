3c2cf86a97324cbc4f3932478839c048370e455d
jed <jed@astera.org>
2023-12-01 09:06:01 -0800

    start complex world

---
+++ games/globo/mods/basenodes/init.lua
@@ -0,0 +1,174 @@
+local WATER_ALPHA = "^[opacity:" .. 160
+local WATER_VISC = 1
+local LAVA_VISC = 7
+
+
+
+minetest.register_node("basenodes:dirt_with_grass", {
+	description = "Dirt with Grass",
+	tiles ={"default_grass.png",
+		-- a little dot on the bottom to distinguish it from dirt
+		"default_dirt.png^basenodes_dirt_with_grass_bottom.png",
+		{name = "default_dirt.png^default_grass_side.png",
+		tileable_vertical = false}},
+	groups = {crumbly=3, soil=1},
+})
+
+
+minetest.register_node("basenodes:dirt", {
+	description = "Dirt",
+	tiles ={"default_dirt.png"},
+	groups = {crumbly=3, soil=1},
+})
+
+minetest.register_node("basenodes:stone", {
+	description = "Stone",
+	tiles = {"default_stone.png"},
+	groups = {cracky=3},
+})
+
+minetest.register_node("basenodes:water_source", {
+	description = "Water Source".."\n"..
+		"Swimmable, spreading, renewable liquid".."\n"..
+		"Drowning damage: 1",
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {"default_water.png"..WATER_ALPHA},
+	special_tiles = {
+		{name = "default_water.png"..WATER_ALPHA, backface_culling = false},
+		{name = "default_water.png"..WATER_ALPHA, backface_culling = true},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "basenodes:water_flowing",
+	liquid_alternative_source = "basenodes:water_source",
+	liquid_viscosity = WATER_VISC,
+	post_effect_color = {a = 64, r = 100, g = 100, b = 200},
+	post_effect_color_shaded = true,
+	groups = {water = 3, liquid = 3},
+})
+
+minetest.register_node("basenodes:water_flowing", {
+	description = "Flowing Water".."\n"..
+		"Swimmable, spreading, renewable liquid".."\n"..
+		"Drowning damage: 1",
+	drawtype = "flowingliquid",
+	waving = 3,
+	tiles = {"default_water_flowing.png"},
+	special_tiles = {
+		{name = "default_water_flowing.png"..WATER_ALPHA,
+			backface_culling = false},
+		{name = "default_water_flowing.png"..WATER_ALPHA,
+			backface_culling = false},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "basenodes:water_flowing",
+	liquid_alternative_source = "basenodes:water_source",
+	liquid_viscosity = WATER_VISC,
+	post_effect_color = {a = 64, r = 100, g = 100, b = 200},
+	post_effect_color_shaded = true,
+	groups = {water = 3, liquid = 3},
+})
+
+minetest.register_node("basenodes:river_water_source", {
+	description = "River Water Source".."\n"..
+		"Swimmable, spreading, non-renewable liquid".."\n"..
+		"Drowning damage: 1",
+	drawtype = "liquid",
+	waving = 3,
+	tiles = { "default_river_water.png"..WATER_ALPHA },
+	special_tiles = {
+		{name = "default_river_water.png"..WATER_ALPHA, backface_culling = false},
+		{name = "default_river_water.png"..WATER_ALPHA, backface_culling = true},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "basenodes:river_water_flowing",
+	liquid_alternative_source = "basenodes:river_water_source",
+	liquid_viscosity = 1,
+	liquid_renewable = false,
+	liquid_range = 2,
+	post_effect_color = {a = 103, r = 30, g = 76, b = 90},
+	post_effect_color_shaded = true,
+	groups = {water = 3, liquid = 3, },
+})
+
+minetest.register_node("basenodes:river_water_flowing", {
+	description = "Flowing River Water".."\n"..
+		"Swimmable, spreading, non-renewable liquid".."\n"..
+		"Drowning damage: 1",
+	drawtype = "flowingliquid",
+	waving = 3,
+	tiles = {"default_river_water_flowing.png"..WATER_ALPHA},
+	special_tiles = {
+		{name = "default_river_water_flowing.png"..WATER_ALPHA,
+			backface_culling = false},
+		{name = "default_river_water_flowing.png"..WATER_ALPHA,
+			backface_culling = false},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "basenodes:river_water_flowing",
+	liquid_alternative_source = "basenodes:river_water_source",
+	liquid_viscosity = 1,
+	liquid_renewable = false,
+	liquid_range = 2,
+	post_effect_color = {a = 103, r = 30, g = 76, b = 90},
+	post_effect_color_shaded = true,
+	groups = {water = 3, liquid = 3, },
+})
+
+minetest.register_node("basenodes:apple", {
+	description = "Apple".."\n"..
+		"Punch: Eat (+2)",
+	drawtype = "plantlike",
+	tiles ={"default_apple.png"},
+	inventory_image = "default_apple.png",
+	paramtype = "light",
+	is_ground_content = false,
+	sunlight_propagates = true,
+	walkable = false,
+	groups = {dig_immediate=3},
+
+	-- Make eatable because why not?
+	on_use = minetest.item_eat(2),
+})
+
+minetest.register_node("basenodes:snowblock", {
+	description = "Snow Block",
+	tiles ={"default_snow.png"},
+	groups = {crumbly=3},
+})
+
+
