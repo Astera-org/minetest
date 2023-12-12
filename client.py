0a837e79d37033e585ec2dd4b8557847883b0ce2
Simon Boehm <simon@siboehm.com>
2023-12-11 16:22:15 -0800

    Extract all command names

--- client.py
+++ client.py
@@ -3,6 +3,91 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
 
+commands = [
+    "FORWARD",
+    "BACKWARD",
+    "LEFT",
+    "RIGHT",
+    "JUMP",
+    "AUX1",
+    "SNEAK",
+    "AUTOFORWARD",
+    "DIG",
+    "PLACE",
+    "ESC",
+    # Other
+    "DROP",
+    "INVENTORY",
+    "CHAT",
+    "CMD",
+    "CMD_LOCAL",
+    "CONSOLE",
+    "MINIMAP",
+    "FREEMOVE",
+    "PITCHMOVE",
+    "FASTMOVE",
+    "NOCLIP",
+    "HOTBAR_PREV",
+    "HOTBAR_NEXT",
+    "MUTE",
+    "INC_VOLUME",
+    "DEC_VOLUME",
+    "CINEMATIC",
+    "SCREENSHOT",
+    "TOGGLE_BLOCK_BOUNDS",
+    "TOGGLE_HUD",
+    "TOGGLE_CHAT",
+    "TOGGLE_FOG",
+    "TOGGLE_UPDATE_CAMERA",
+    "TOGGLE_DEBUG",
+    "TOGGLE_PROFILER",
+    "CAMERA_MODE",
+    "INCREASE_VIEWING_RANGE",
+    "DECREASE_VIEWING_RANGE",
+    "RANGESELECT",
+    "ZOOM",
+    "QUICKTUNE_NEXT",
+    "QUICKTUNE_PREV",
+    "QUICKTUNE_INC",
+    "QUICKTUNE_DEC",
+    # hotbar
+    "SLOT_1",
+    "SLOT_2",
+    "SLOT_3",
+    "SLOT_4",
+    "SLOT_5",
+    "SLOT_6",
+    "SLOT_7",
+    "SLOT_8",
+    "SLOT_9",
+    "SLOT_10",
+    "SLOT_11",
+    "SLOT_12",
+    "SLOT_13",
+    "SLOT_14",
+    "SLOT_15",
+    "SLOT_16",
+    "SLOT_17",
+    "SLOT_18",
+    "SLOT_19",
+    "SLOT_20",
+    "SLOT_21",
+    "SLOT_22",
+    "SLOT_23",
+    "SLOT_24",
+    "SLOT_25",
+    "SLOT_26",
+    "SLOT_27",
+    "SLOT_28",
+    "SLOT_29",
+    "SLOT_30",
+    "SLOT_31",
+    "SLOT_32",
+    # Fake keycode for array size and internal checks
+    "INTERNAL_ENUM_COUNT",
+]
+
+
 def display_image():
     # Wait for the response from the server
     response = socket.recv()
@@ -11,7 +96,9 @@ def display_image():
     img_data = np.frombuffer(response, dtype=np.uint8)
 
     # Reshape the numpy array to the correct dimensions
-    img_data = img_data.reshape((1051, 1728, 3))  # replace height and width with the actual values
+    img_data = img_data.reshape(
+        (1051, 1728, 3)
+    )  # replace height and width with the actual values
 
     # Convert the numpy array to a PIL Image
     img = Image.fromarray(img_data)
@@ -26,7 +113,7 @@ def display_image():
 
 def send_key(event):
     key = event.char.upper()
-    
+
     # Ensure that only the keys we want are sent
     if key in ["Q", "W", "A", "S", "D", " "]:
         # Send the message to the server
@@ -36,7 +123,7 @@ def send_key(event):
         window.quit()
     else:
         raise NotImplementedError()
-    
+
     display_image()
 
 
