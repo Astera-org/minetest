f7d57a7999b487869dba10f49305b8dd446e2450
Simon Boehm <mail@siboehm.com>
2024-01-14 18:28:28 +0000

    ci

--- util/ci/build.sh
+++ util/ci/build.sh
@@ -2,11 +2,14 @@
 
 set -x
 
+SDL2_DIR=$(pwd)/lib/SDL/build/lib/cmake/SDL2/
+ls -l $SDL2_DIR
+
 cmake -B build -S . \
 	-DCMAKE_FIND_FRAMEWORK=LAST \
 	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} \
 	-DRUN_IN_PLACE=TRUE \
-	-DSDL2_DIR=lib/SDL/build/lib/cmake/SDL2/ \
+	-DSDL2_DIR=$SDL2_DIR \
 	-DENABLE_GETTEXT=${CMAKE_ENABLE_GETTEXT:-TRUE} \
 	-DBUILD_SERVER=${CMAKE_BUILD_SERVER:-TRUE} \
 	${CMAKE_FLAGS}
