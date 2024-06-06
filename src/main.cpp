7f8af09ed3c2c8f127d8d11f90416e62b48d2230
Gary Miguel <garymm@astera.org>
2024-06-06 16:13:39 -0700

    restore world_path

--- src/main.cpp
+++ src/main.cpp
@@ -924,6 +924,8 @@ static bool auto_select_world(GameParams *game_params)
 	std::string world_path;
 
 	if (!game_params->game_spec.id.empty()) {
+		infostream << "Game specified but world was not. Creating new world" << std::endl;
+
 		std::time_t t = std::time(nullptr);
 		char date_chars[100];
 		std::strftime(date_chars, sizeof(date_chars), "%F-%H-%M-%S", std::localtime(&t));
@@ -933,12 +935,13 @@ static bool auto_select_world(GameParams *game_params)
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> dis(0, 0xF);
-
-		world_name += "-";
+		char rand_buf[5];
 		for (int i = 0; i < 5; ++i) {
 			int random_num = dis(gen);
-			world_name += "0123456789ABCDEF"[random_num];
+			rand_buf[i] = "0123456789ABCDEF"[random_num];
 		}
+		world_name += "-" + std::string(rand_buf);
+		world_path = porting::path_user + DIR_DELIM + "worlds" + DIR_DELIM + world_name;
 
 		try {
 			world_path = loadGameConfAndInitWorld(
