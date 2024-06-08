6f3ffee2010747ba91a89532cbaac40bb19df69f
DrJessop <andrewgrebenisan@gmail.com>
2024-06-07 17:26:15 -0700

    Set enable security to false at compile time since not allowing it from conf file in games

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -429,7 +429,7 @@ void set_default_settings()
 	settings->setDefault("emergequeue_limit_diskonly", "128");
 	settings->setDefault("emergequeue_limit_generate", "128");
 	settings->setDefault("num_emerge_threads", "1");
-	settings->setDefault("secure.enable_security", "true");
+	settings->setDefault("secure.enable_security", "false");
 	settings->setDefault("secure.trusted_mods", "");
 	settings->setDefault("secure.http_mods", "");
 
