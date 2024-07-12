a186dfebd2ab5a6ca97b99b1f02f048cfeb62065
Eric Alt <ericallenalt@gmail.com>
2024-07-12 12:34:59 -0700

    Refactor gym_client
    Create GymClient class that can be initialized with a gym env

--- python/tools/gym_client.py
+++ python/tools/gym_client.py
@@ -1,14 +1,13 @@
 import argparse
-import shutil
-import signal
 import sys
-import tempfile
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Any, Callable
 
 import gymnasium as gym
 import numpy as np
 import pygame
+from pygame.event import Event
 
 from minetest.minetest_env import INVERSE_KEY_MAP, KEY_MAP
 
@@ -23,10 +22,10 @@ KEY_TO_KEYTYPE = {
     "K": "place",
 }
 ARROW_KEYS_TO_MOUSE_DIRECTION = {
-    "UP": (0, -20),
-    "DOWN": (0, 20),
-    "LEFT": (-20, 0),
-    "RIGHT": (20, 0),
+    "UP": (0, -1),
+    "DOWN": (0, 1),
+    "LEFT": (-1, 0),
+    "RIGHT": (1, 0),
 }
 
 
@@ -36,91 +35,97 @@ class Mouse:
     dy: float
 
 
-def handle_key_event(event):
-    key = pygame.key.name(event.key).upper()
-
-    if event.type == pygame.KEYDOWN:
-        if key in KEY_TO_KEYTYPE:
-            keys_down.add(key)
-        if key in ARROW_KEYS_TO_MOUSE_DIRECTION:
-            mouse.dx += ARROW_KEYS_TO_MOUSE_DIRECTION[key][0]
-            mouse.dy += ARROW_KEYS_TO_MOUSE_DIRECTION[key][1]
-    elif event.type == pygame.KEYUP:
-        if key in KEY_TO_KEYTYPE and key in keys_down:
-            keys_down.remove(key)
-        if key in ARROW_KEYS_TO_MOUSE_DIRECTION:
-            mouse.dx -= ARROW_KEYS_TO_MOUSE_DIRECTION[key][0]
-            mouse.dy -= ARROW_KEYS_TO_MOUSE_DIRECTION[key][1]
-
-
-def get_action_from_key_cache(key_cache, mouse):
+def get_action_from_key_cache(
+    key_cache: set[str], mouse: Mouse
+) -> dict[str, np.ndarray]:
     keys = np.zeros(len(KEY_MAP), dtype=bool)
     for key in key_cache:
-        keys[INVERSE_KEY_MAP[KEY_TO_KEYTYPE[key]]] = True
+        if key in KEY_TO_KEYTYPE:
+            keys[INVERSE_KEY_MAP[KEY_TO_KEYTYPE[key]]] = True
 
     mouse = np.array([mouse.dx, mouse.dy])
     return {"keys": keys, "mouse": mouse}
 
 
-def game_loop():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+class GymClient:
+    def __init__(
+        self,
+        env: gym.Env,
+        mouse_magnitude: int = 20,
+        get_action_from_key_cache: Callable[
+            [set[str], Mouse], dict[str, np.ndarray]
+        ] = get_action_from_key_cache,
+    ):
+        self._env = env
+        self._keys_down = set()
+        self._mouse = Mouse(0, 0)
+        self._mouse_magnitude = mouse_magnitude
+        self._get_action_from_key_cache = get_action_from_key_cache
+
+    def __enter__(self):
+        pygame.init()
+        self._env.reset()
+        return self
+
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        self._env.close()
+        pygame.quit()
+
+    def game_loop(self) -> None:
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type in [pygame.KEYDOWN, pygame.KEYUP]:
+                    self._handle_key_event(event)
+
+            action = self._get_action_from_key_cache(self._keys_down, self._mouse)
+            state, reward, terminated, truncated, info = env.step(action)
+            env.render()
+            print(reward)
+
+            if terminated or truncated:
+                print("\n\n--TERMINATED--\n\n")
                 running = False
-            elif event.type in [pygame.KEYDOWN, pygame.KEYUP]:
-                handle_key_event(event)
 
-        action = get_action_from_key_cache(keys_down, mouse)
-        state, reward, terminated, truncated, info = env.step(action)
-        env.render()
-        print(reward)
+    def _handle_key_event(self, event: Event) -> None:
+        key = pygame.key.name(event.key).upper()
 
-        if terminated:
-            print("\n\n--TERMINATED--\n\n")
-            running = False
+        if event.type == pygame.KEYDOWN:
+            if key in KEY_TO_KEYTYPE:
+                self._keys_down.add(key)
+            if key in ARROW_KEYS_TO_MOUSE_DIRECTION:
+                self._mouse.dx += (
+                    ARROW_KEYS_TO_MOUSE_DIRECTION[key][0] * self._mouse_magnitude
+                )
+                self._mouse.dy += (
+                    ARROW_KEYS_TO_MOUSE_DIRECTION[key][1] * self._mouse_magnitude
+                )
+        elif event.type == pygame.KEYUP:
+            if key in KEY_TO_KEYTYPE and key in self._keys_down:
+                self._keys_down.remove(key)
+            if key in ARROW_KEYS_TO_MOUSE_DIRECTION:
+                self._mouse.dx -= (
+                    ARROW_KEYS_TO_MOUSE_DIRECTION[key][0] * self._mouse_magnitude
+                )
+                self._mouse.dy -= (
+                    ARROW_KEYS_TO_MOUSE_DIRECTION[key][1] * self._mouse_magnitude
+                )
 
 
-def signal_handler(sig, frame):
-    env.close()
-    pygame.quit()
-
-
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument(
-    "--server_addr",
-    type=str,
-    default=None,
-    help=(
-        "Minetest host:port to connect to. If set, will not "
-        "start minetest (will assume it's already running)."
-    ),
-)
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-
-    keys_down = set()
-    mouse = Mouse(0, 0)
-    pygame.init()
-
+def get_gym_args(__file__, args):
     gym_make_args = {
         "id": "minetest-v0",
         "render_mode": "human",
         "display_size": (512, 512),
     }
-    args = arg_parser.parse_args()
     if args.server_addr:
         gym_make_args["server_addr"] = args.server_addr
         gym_make_args["executable"] = None
     else:
-        original_world_dir = (
-            Path(__file__).parent / "tests" / "worlds" / "test_world_minetestenv"
-        )
-        temp_dir = tempfile.TemporaryDirectory()
-        temp_world_dir = Path(temp_dir) / "test_world_minetestenv"
-        shutil.copytree(original_world_dir, temp_world_dir)
-        gym_make_args["world_dir"] = temp_world_dir
+        game_dir = "/Users/ericalt/Documents/minetest/games/devtest"
+        gym_make_args["game_dir"] = game_dir
         # The Makefile puts the binary into build/macos
         repo_root = Path(__file__).parent.parent.parent
         minetest_executable = repo_root / "bin" / "minetest"
@@ -134,10 +139,25 @@ if __name__ == "__main__":
                 / "MacOS"
                 / "minetest"
             )
+            # minetest_executable = "/opt/homebrew/bin/minetest"
         gym_make_args["executable"] = minetest_executable
         gym_make_args["headless"] = False
+        gym_make_args["verbose_logging"] = True
+    return gym_make_args
 
-    with gym.make(**gym_make_args) as env:
-        env.reset()
-        game_loop()
-    pygame.quit()
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--server_addr",
+        type=str,
+        default=None,
+        help=(
+            "Minetest host:port to connect to. If set, will not "
+            "start minetest (will assume it's already running)."
+        ),
+    )
+    args = arg_parser.parse_args()
+    gym_make_args = get_gym_args(__file__, args)
+    with gym.make(**gym_make_args) as env, GymClient(env) as client:
+        client.game_loop()
