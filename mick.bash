35167997a775fdde00fcc728e2a087fa3a901071
Mick van Gelderen <mickvangelderen@gmail.com>
2024-11-19 15:07:21 -0800

    Update mick.bash

--- mick.bash
+++ mick.bash
@@ -5,26 +5,30 @@ set -euo pipefail
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 
 build_with_compiler() {
-	export CC="$1"
-    export CXX="$2"
+    local cc="$1"
+    local cxx="$2"
+    local san="$3"
 
-    local build_dir="build-$CXX-$3"
+    export CC="$cc"
+    export CXX="$cxx"
+
+    local build_dir="build-$cxx-$san"
 
     cmake -B "$build_dir" -S . \
-		-DCMAKE_C_COMPILER="$1" \
-		-DCMAKE_CXX_COMPILER="$2" \
-		-DCMAKE_FIND_FRAMEWORK=LAST \
-		-DRUN_IN_PLACE=TRUE \
-		-DENABLE_SOUND=FALSE \
-		-DENABLE_GETTEXT=TRUE \
-		-GNinja \
-		-DUSE_SDL2=ON \
-		-DSANITIZER="$3" \
-		-DCMAKE_CXX_LINK_FLAGS="-fuse-ld=mold" \
-		-DCMAKE_BUILD_TYPE=Debug \
-		-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-		-DCMAKE_COLOR_DIAGNOSTICS=ON \
-		-DBUILD_SERVER=ON
+        -DCMAKE_C_COMPILER="$cc" \
+        -DCMAKE_CXX_COMPILER="$cxx" \
+        -DCMAKE_FIND_FRAMEWORK=LAST \
+        -DRUN_IN_PLACE=TRUE \
+        -DENABLE_SOUND=FALSE \
+        -DENABLE_GETTEXT=TRUE \
+        -GNinja \
+        -DUSE_SDL2=ON \
+        -DSANITIZER="$3" \
+        -DCMAKE_CXX_LINK_FLAGS="-fuse-ld=mold" \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+        -DCMAKE_COLOR_DIAGNOSTICS=ON \
+        -DBUILD_SERVER=ON
 
     cmake --build "$build_dir" -j "$(nproc)"
 }
@@ -33,5 +37,5 @@ build_with_compiler gcc-14 g++-14 none
 # build_with_compiler gcc-14 g++-14 ubsan
 # build_with_compiler gcc-14 g++-14 asan
 build_with_compiler clang-18 clang++-18 none
-build_with_compiler clang-18 clang++-18 ubsan
-build_with_compiler clang-18 clang++-18 asan
+# build_with_compiler clang-18 clang++-18 ubsan
+# build_with_compiler clang-18 clang++-18 asan
